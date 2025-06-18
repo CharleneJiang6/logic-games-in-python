a19fd1d3eea64d8786241b85fb76cf7f5e5d5a7a
Charlene Jiang <charlene.jiang06@gmail.com>
2025-06-18 11:57:31 +0200

    maj tp4 morpion, au niveau de minmax

--- tp4/tp4.py
+++ tp4/tp4.py
@@ -1,12 +1,15 @@
+import ast
 from typing import Callable
 import random
 import math
 import itertools
 
-Grid = tuple[tuple[int, ...], ...]
-State = Grid
-Action = tuple[int, int]
-Player = int
+# from pprint import pprint
+
+Grid = tuple[tuple[int, ...], ...]  # eg: ((X,O,X),(X,X,O),(O,X,X))
+State = Grid  # un etat du jeu
+Action = tuple[int, int]  # eg: (0,1)
+Player = int  # eg: X,O
 Score = float
 
 Strategy = Callable[[State, Player], Action]  # pb
@@ -16,6 +19,35 @@ DRAW = 0
 EMPTY = 0
 X = 1
 O = 2
+
+EMPTY_GRID: Grid = ((0, 0, 0), (0, 0, 0), (0, 0, 0))
+GRID_0: Grid = EMPTY_GRID
+GRID_1: Grid = ((0, 0, 0), (0, X, O), (0, 0, 0))
+# (0, 0, 0),
+# (0, X, O),
+# (0, 0, 0))
+
+GRID_2: Grid = ((O, 0, X), (X, X, O), (O, X, 0))
+# ((O, 0, X),
+# (X, X, O),
+# (O, X, 0))
+
+GRID_3: Grid = ((O, 0, X), (0, X, O), (O, X, 0))
+# ((O, 0, X),
+# (0, X, O),
+# (O, X, 0))
+
+GRID_4: Grid = ((0, 0, 0), (X, X, O), (0, 0, 0))
+# ((0, 0, 0),
+# (X, X, O),
+# (0, 0, 0))
+
+GRID_5: Grid = ((X, 0, 0), (X, X, O), (0, 0, X))
+# ((0, 0, 0),
+# (X, X, O),
+# (0, 0, 0))
+
+GRID_6: Grid = ((X, O, O), (X, 0, O), (X, 0, X))
 
 
 def grid_tuple_to_grid_list(grid: Grid) -> list[list[int]]:
@@ -28,35 +60,16 @@ def grid_list_to_grid_tuple(grid: list[list[int]]) -> Grid:
 
 
 def legals(grid: State) -> list[Action]:
-    if type(grid) != Grid:
-        g = grid_tuple_to_grid_list(grid)
-    else:
-        g = grid
-
-    # compter le nombre de X et O pour savoir cest a qui de jouer
-    # nbO=nbX=0
-    # current = 0
     actions = []
-
-    # for line in g:
-    #     for cell in line:
-    #         if cell == 1:
-    #             X+=1
-    #         elif cell==2:
-    #             O+=1
-    # if nbO==nbX:
-    #     current=1
-    #     print("Voici les actions qui s'offrent à X : ")
-    # else:
-    #     current=2
-    #     print("Voici les actions qui s'offrent à O : ")
-
     for i in range(3):
         for j in range(3):
-            if g[i][j] == EMPTY:
+            if grid[i][j] == EMPTY:
                 actions.append((i, j))
 
     return actions
+
+
+# print(legals(GRID_0))
 
 
 def diagonal(grid: State, player: Player) -> bool:
@@ -68,42 +81,36 @@ def anti_diagonal(grid: State, player: Player) -> bool:
 
 
 def line(grid: State, player: Player) -> bool:
-    # TODO: mettre un Flag pour ne pas avoir tout parcourir
-
-    g = grid_list_to_grid_tuple(grid)
-
     # verif lignes
     ligne = (player, player, player)
-    for row in g:
-        if row == ligne:
-            return True
+    if any(row == ligne for row in grid):
+        return True
 
     # verif col
     for i in range(3):
-        if g[0][i] == g[1][i] == g[2][i] == player:
+        # if all(grid[j][i] == player for j in range(3)):
+        if grid[0][i] == grid[1][i] == grid[2][i] == player:
             return True
 
-    return diagonal(g, player) or anti_diagonal(g, player)
+    return diagonal(grid, player) or anti_diagonal(grid, player)
 
     return False
 
 
+# print(line(GRID_5, X))
+
 def final(grid: State) -> bool:
-    # for row in grid:
-    #     for cell in row:
-    #         if cell is EMPTY:
-    #             return False
-
-    # return True
-
+    """si on est dans un etat terminal : gain ou plus de cases vides"""
     return line(grid, X) or line(grid, O) or len(legals(grid)) == 0
 
 
+# print(final(GRID_6))
+
 def score(grid: State) -> Score:
-    """en supposant que le score est à donner pour X.
+    """
+    en supposant que le score est à donner pour X.
     alors, si c'est O qui forme une ligne, alors on affiche -1
     """
-
     if line(grid, X):
         return 1
     if line(grid, O):
@@ -111,33 +118,37 @@ def score(grid: State) -> Score:
     return 0
 
 
-## TODO : a revoir pourquoi ca ne marche pas
-# def pprint(grid: State):
-#     """affichage de la grille de jeu"""
-#     for row in grid:
-#         for cell in grid:
-#             if cell[0] == EMPTY or cell[0] == 0:
-#                 print('.', end=" ")
-#             elif cell[0] == X or cell == 1:
-#                 print("X", end= " ")
-#             elif cell[0] == O or cell == 2:
-#                 print("O", end= " ")
-#         print()
-
-
 def pprint(grid: State):
-    for i in range(3):
-        for j in range(3):
-            if grid[i][j] == X:
+    """affichage de la grille de jeu"""
+    for row in grid:
+        for cell in row:
+            if cell in (EMPTY, 0):
+                print('.', end=" ")
+            elif cell in (X, 1):
                 print("X", end=" ")
-            elif grid[i][j] == O:
+            elif cell in (O, 2):
                 print("O", end=" ")
-            else:
-                print(".", end=" ")
         print()
 
 
+# pprint(GRID_6)
+# pprint(GRID_3)
+
+
+# def pprint(grid: State):
+#     for i in range(3):
+#         for j in range(3):
+#             if grid[i][j] == X:
+#                 print("X", end=" ")
+#             elif grid[i][j] == O:
+#                 print("O", end=" ")
+#             else:
+#                 print(".", end=" ")
+#         print()
+
+
 def play(grid: State, player: Player, action: Action) -> State:
+    """met a jour la grille avec l'action du joueur"""
     if action not in legals(grid):
         return grid
 
@@ -147,33 +158,51 @@ def play(grid: State, player: Player, action: Action) -> State:
     return grid_list_to_grid_tuple(g)
 
 
-# def play(grid: State, player: Player, action: Action) -> State:
-#     g=[list(l) for l in grid]
-#     g[action[0]][action[1]]=player
-#     return tuple(map(tuple,g))
-
+# UNE STRATEGIE RENVOIE L ACTION CHOISIE PAR LE JOUEUR
 
 def strategy_brain(grid: State, player: Player) -> Action:
-    print("à vous de jouer: ", end="")
-    s = input()
+    """renvoie le tuple (action) saisi par le player"""
+    player_str = "X" if player == X else "O"
+    print(f"\nA vous de jouer, placez un {player_str} sur une case (x,y) : ", end="")
+    s = input()  # eg: (0,2)
     print()
-    t = ast.literal_eval(s)
+    t = ast.literal_eval(s)  # arbre syntaxique abstrait
+    # evaluer la chaine et le convertit en obj python correspondant
 
-    return t
+    return t  # un tuple (0,2)
 
 
 def tictactoe(strategy_X: Strategy, strategy_O: Strategy, debug: bool = False) -> Score:
-    g = ((0, 0, 0), (0, 0, 0), (0, 0, 0))
+    """boucle du jeu jusqu'a determiner le gagnant et le score"""
+    g = EMPTY_GRID
+    if debug:
+        pprint(g)
+
+    current_player = X
+    strategies = {X: strategy_X, O: strategy_O}
 
     while not final(g):
-        action = strategy_X(g, X)
-        g = play(g, X, action)
+        # Joueur courant joue
+        action = strategies[current_player](g, current_player)
+        g = play(g, current_player, action)
+        if debug:
+            pprint(g)
 
-        action = strategy_O(g, O)
-        g = play(g, O, action)
-
-        if line(g, X) or line(g, O):
+        # Test si fin
+        if line(g, current_player):
+            if debug:
+                player_str = "X" if current_player == X else "O"
+                print(f"Fin du jeu ! Le joueur {player_str} gagne.")
             return score(g)
+
+        current_player = O if current_player == X else X
+
+    if debug:
+        print("Match nul !")
+    return score(g)
+
+
+# tictactoe(strategy_brain, strategy_brain, debug=True)
 
 
 def strategy_first_legal(grid: State, player: Player) -> Action:
@@ -182,30 +211,85 @@ def strategy_first_legal(grid: State, player: Player) -> Action:
 
 def strategy_random(grid: State, player: Player) -> Action:
     actions = legals(grid)
-    i = random.randrange(0, len(actions))
+    i = random.randrange(0, len(actions))  # random.randint (borne incluse), mais randrange exclu borne
     return actions[i]
     # return random.choices(actions)
 
 
-# IMPLEMENTATION MIN MAX
+### MIN MAX ###
 
 
-##TODO: tester
 def minmax(grid: State, player: Player) -> Score:
+    """avec l'etat courant du jeu, donne le meilleur score possible"""
     if final(grid):
         return score(grid)
 
-    if player == X:  # X, maximizing Player
+    if player == X:  # X, maximizing player
         value = -math.inf
-        for etat in legals(grid):
-            value = max(value, minmax(etat, O))
+        for action in legals(grid):
+            g = play(grid, X, action)
+            value = max(value, minmax(g, O))
+        return value
+    else:  # O, minimizing player
+        value = math.inf
+        for action in legals(grid):
+            g = play(grid, O, action)
+            value = min(value, minmax(g, X))
         return value
 
-    else:  # O, minimizing Player
-        value = math.inf
-        for etat in legals(grid):
-            value = max(value, minmax(etat, X))
+
+# print((minmax(GRID_0,X))) # match nul si joeurs normaux
+# print((minmax(GRID_6, O)))  # oui, gain pour X : res = 1
+
+
+def minmax_depth(grid: State, player: Player, depth=9) -> Score:
+    """avec l'etat courant du jeu, donne le meilleur score possible, en limitant la profondeur de recherche"""
+    if depth == 0 or final(grid):
+        return score(grid)
+
+    if player == X:  # X, maximizing player
+        value = -math.inf
+        for action in legals(grid):
+            g = play(grid, X, action)
+            value = max(value, minmax_depth(g, O, depth - 1))
         return value
+    else:  # O, minimizing player
+        value = math.inf
+        for action in legals(grid):
+            g = play(grid, O, action)
+            value = min(value, minmax_depth(g, X, depth - 1))
+        return value
+
+
+# print(minmax_depth(GRID_0, O, depth=3))
+# print(minmax_depth(GRID_6, X, depth=3)) # meme resultat que minmax mais + rapide
+
+# TODO !!
+def evaluation(grid: State, player: Player) -> Score:
+    """pre """
+    pass
+
+
+def minmax_eval(grid: State, player: Player, depth=9) -> Score:
+    """
+    fonction d'évaluation: au lieu d'appeler score(), on evalue l'état pour estimer sa veleur
+    """
+    if depth == 0 or final(grid):
+        return evaluation(grid, player)  # Utilise la fonction d’évaluation
+
+    if player == X:  # Maximizing
+        value = -math.inf
+        for action in legals(grid):
+            g = play(grid, X, action)
+            value = max(value, minmax_eval(g, O, depth-1))
+        return value
+    else:  # Minimizing
+        value = math.inf
+        for action in legals(grid):
+            g = play(grid, O, action)
+            value = min(value, minmax_eval(g, X, depth-1))
+        return value
+
 
 
 # https://papers-100-lines.medium.com/the-minimax-algorithm-and-alpha-beta-pruning-tutorial-in-30-lines-of-python-code-e4a3d97fa144
@@ -239,7 +323,7 @@ def strategy_minmax(grid: State, player: Player) -> Action:
 
 
 def minmax_actions(
-    grid: State, player: Player, depth: int = 0
+        grid: State, player: Player, depth: int = 0
 ) -> tuple[Score, list[Action]]:
     # action: tuple[int,int] = None
     actions = []
@@ -358,4 +442,4 @@ g4 = ((0, 0, 0), (X, X, O), (0, 0, 0))
 
 # print(minmax(m, X))  #!! ERROR
 
-print(minmax_action(m, X, 4))
+# print(minmax_action(m, X, 4))
